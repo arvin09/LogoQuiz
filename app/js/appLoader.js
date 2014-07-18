2e100ea57530ec2abf01d3f729535a5de83012c9
Arvind Waghmare <Arvind.Waghmare@morningstar.com>
2014-07-18 10:27:27 +0530

    1. Adding animation to screens 2. Level initial logic 3 . Design for Levels

--- app/js/appLoader.js
+++ app/js/appLoader.js
@@ -2,213 +2,267 @@ define(function(require){
 	var quiz = require('js/logoquiz');
 
 	var logoData = {
-			logos : [
-			         {
-			        	 id :'1',
-			        	 name: 'logo1',
-			        	 star:1,
-			        	 ans : 'starbucks',
-			        	 done : false
-			         },
-			         
-			         {
-			        	 id : '2',
-			        	 name: 'logo2',
-			        	 star:2,
-			        	 ans : '3mobile',
-			             done : false
-			         },
 
-			         {
-			        	 id: '3',
-			        	 name: 'logo3',
-			        	 star:1,
-			        	 ans:'logitech',
-			             done : false
-			         },
+			levels :[
+			        {
+			        	id:'1',
+			        	name:'Level 1',
+			        	pointScored: '30',
+			        	completedCount:'10',
+			        	totalLogo:'30',
+			        	bgcolor:'red',
+			        	started:true,
+			        },
+			        {
 
-			         {
-			        	 id: '4',
-			        	 name: 'logo4',
-			        	 star:3,
-			        	 ans:'volkswagen',
-			             done : false
-			         },
+			        	id:'2',
+			        	name:'Level 2',
+			        	pointScored: '30',
+			        	completedCount:'10',
+			        	totalLogo:'30',
+			        	bgcolor:'rgb(3, 199, 226)',
+			        	started:true,
+			        },
+			        {
+			        	id:'3',
+			        	name:'Level 3',
+			        	pointScored: '30',
+			        	completedCount:'10',
+			        	totalLogo:'30',
+			        	bgcolor:'rgb(160, 207, 9)',
+			        	started:true,
 
-			         {
-			        	 id: '5',
-			        	 name: 'logo5',
-			        	 star:3,
-			        	 ans:'network',
-			             done : false
-			         },
+			        },
+			        {
+			        	id:'4',
+			        	name:'Level 4',
+			        	pointScored: '0',
+			        	completedCount:'0',
+			        	totalLogo:'30',
+			        	bgcolor:'rgb(255, 112, 0)',
+			        	started:false,
+			        },
+			        {
+			        	id:'5',
+			        	name:'Level 5',
+			        	pointScored: '0',
+			        	completedCount:'0',
+			        	totalLogo:'30',
+			        	bgcolor:'rgb(129, 90, 41)',
+			        	started:false,
 
-			         {
-			        	 id: '6',
-			        	 name: 'logo6',
-			        	 star:3,
-			        	 ans:'adidas',
-			             done : false
-			         },
-			         {
-			        	 id: '7',
-			        	 name: 'logo7',
-			        	 star: 2,
-			        	 ans : 'mozilla firefox',
-			             done : false
-			         },
-			         {	 id: '8',
-			        	 name: 'logo1',
-			        	 star:1,
-			        	 ans : 'starbucks',
-			        	 done : false
-			         },
+			        }
+			        ],
 
-			         {
-			        	 id: '9',
-			        	 name: 'logo2',
-			        	 star:2,
-			        	 ans : '3mobile',
-			             done : false
-			         },
 
-			         {
-			        	 id: '10',
-			        	 name: 'logo3',
-			        	 star:1,
-			        	 ans:'logitech',
-			             done : false
-			         },
 
-			         {
-			        	 id: '11',
-			        	 name: 'logo4',
-			        	 star:3,
-			        	 ans:'volkswagen',
-			             done : false
-			         },
+			        logos : [
+			                 {
+			                	 id :'1',
+			                	 name: 'logo1',
+			                	 star:1,
+			                	 ans : 'starbucks',
+			                	 done : false
+			                 },
 
-			         {
-			        	 id: '12',
-			        	 name: 'logo5',
-			        	 star:3,
-			        	 ans:'network',
-			             done : false
-			         },
+			                 {
+			                	 id : '2',
+			                	 name: 'logo2',
+			                	 star:2,
+			                	 ans : '3mobile',
+			                	 done : false
+			                 },
 
-			         {
-			        	 id: '13',
-			        	 name: 'logo6',
-			        	 star:3,
-			        	 ans:'adidas',
-			             done : false
-			         },
-			         {
-			        	 id: '14',
-			        	 name: 'logo7',
-			        	 star: 2,
-			        	 ans : 'mozilla firefox',
-			             done : false
-			         },			         {
-			        	 id: '15',
-			        	 name: 'logo1',
-			        	 star:1,
-			        	 ans : 'starbucks',
-			        	 done : false
-			         },
+			                 {
+			                	 id: '3',
+			                	 name: 'logo3',
+			                	 star:1,
+			                	 ans:'logitech',
+			                	 done : false
+			                 },
 
-			         {
-			        	 id: '16',
-			        	 name: 'logo2',
-			        	 star:2,
-			        	 ans : '3mobile',
-			             done : false
-			         },
+			                 {
+			                	 id: '4',
+			                	 name: 'logo4',
+			                	 star:3,
+			                	 ans:'volkswagen',
+			                	 done : false
+			                 },
 
-			         {
-			        	 id: '17',
-			        	 name: 'logo3',
-			        	 star:1,
-			        	 ans:'logitech',
-			             done : false
-			         },
+			                 {
+			                	 id: '5',
+			                	 name: 'logo5',
+			                	 star:3,
+			                	 ans:'network',
+			                	 done : false
+			                 },
 
-			         {
-			        	 id: '18',
-			        	 name: 'logo4',
-			        	 star:3,
-			        	 ans:'volkswagen',
-			             done : false
-			         },
+			                 {
+			                	 id: '6',
+			                	 name: 'logo6',
+			                	 star:3,
+			                	 ans:'adidas',
+			                	 done : false
+			                 },
+			                 {
+			                	 id: '7',
+			                	 name: 'logo7',
+			                	 star: 2,
+			                	 ans : 'mozilla firefox',
+			                	 done : false
+			                 },
+			                 {	 id: '8',
+			                	 name: 'logo1',
+			                	 star:1,
+			                	 ans : 'starbucks',
+			                	 done : false
+			                 },
 
-			         {
-			        	 id: '19',
-			        	 name: 'logo5',
-			        	 star:3,
-			        	 ans:'network',
-			             done : false
-			         },
+			                 {
+			                	 id: '9',
+			                	 name: 'logo2',
+			                	 star:2,
+			                	 ans : '3mobile',
+			                	 done : false
+			                 },
 
-			         {
-			        	 id: '20',
-			        	 name: 'logo6',
-			        	 star:3,
-			        	 ans:'adidas',
-			             done : false
-			         },
-			         {
-			        	 id: '21',
-			        	 name: 'logo7',
-			        	 star: 2,
-			        	 ans : 'mozilla firefox',
-			             done : false
-			         },			         {
-			        	 id: '22',
-			        	 name: 'logo1',
-			        	 star:1,
-			        	 ans : 'starbucks',
-			        	 done : false
-			         },
+			                 {
+			                	 id: '10',
+			                	 name: 'logo3',
+			                	 star:1,
+			                	 ans:'logitech',
+			                	 done : false
+			                 },
 
-			         {
-			        	 id: '23',
-			        	 name: 'logo2',
-			        	 star:2,
-			        	 ans : '3mobile',
-			             done : false
-			         },
+			                 {
+			                	 id: '11',
+			                	 name: 'logo4',
+			                	 star:3,
+			                	 ans:'volkswagen',
+			                	 done : false
+			                 },
 
-			         {
-			        	 id: '24',
-			        	 name: 'logo3',
-			        	 star:1,
-			        	 ans:'logitech',
-			             done : false
-			         },
+			                 {
+			                	 id: '12',
+			                	 name: 'logo5',
+			                	 star:3,
+			                	 ans:'network',
+			                	 done : false
+			                 },
 
-			         {
-			        	 id: '25',
-			        	 name: 'logo4',
-			        	 star:3,
-			        	 ans:'volkswagen',
-			             done : false
-			         },
+			                 {
+			                	 id: '13',
+			                	 name: 'logo6',
+			                	 star:3,
+			                	 ans:'adidas',
+			                	 done : false
+			                 },
+			                 {
+			                	 id: '14',
+			                	 name: 'logo7',
+			                	 star: 2,
+			                	 ans : 'mozilla firefox',
+			                	 done : false
+			                 },			         {
+			                	 id: '15',
+			                	 name: 'logo1',
+			                	 star:1,
+			                	 ans : 'starbucks',
+			                	 done : false
+			                 },
 
-			         {
-			        	 id: '26',
-			        	 name: 'logo5',
-			        	 star:3,
-			        	 ans:'network',
-			             done : false
-			         },
+			                 {
+			                	 id: '16',
+			                	 name: 'logo2',
+			                	 star:2,
+			                	 ans : '3mobile',
+			                	 done : false
+			                 },
 
-			         {
-			        	 id: '27',
-			        	 name: 'logo6',
-			        	 star:3,
-			        	 ans:'adidas',
-			             done : false
-			         }			         ]
+			                 {
+			                	 id: '17',
+			                	 name: 'logo3',
+			                	 star:1,
+			                	 ans:'logitech',
+			                	 done : false
+			                 },
+
+			                 {
+			                	 id: '18',
+			                	 name: 'logo4',
+			                	 star:3,
+			                	 ans:'volkswagen',
+			                	 done : false
+			                 },
+
+			                 {
+			                	 id: '19',
+			                	 name: 'logo5',
+			                	 star:3,
+			                	 ans:'network',
+			                	 done : false
+			                 },
+
+			                 {
+			                	 id: '20',
+			                	 name: 'logo6',
+			                	 star:3,
+			                	 ans:'adidas',
+			                	 done : false
+			                 },
+			                 {
+			                	 id: '21',
+			                	 name: 'logo7',
+			                	 star: 2,
+			                	 ans : 'mozilla firefox',
+			                	 done : false
+			                 },			         {
+			                	 id: '22',
+			                	 name: 'logo1',
+			                	 star:1,
+			                	 ans : 'starbucks',
+			                	 done : false
+			                 },
+
+			                 {
+			                	 id: '23',
+			                	 name: 'logo2',
+			                	 star:2,
+			                	 ans : '3mobile',
+			                	 done : false
+			                 },
+
+			                 {
+			                	 id: '24',
+			                	 name: 'logo3',
+			                	 star:1,
+			                	 ans:'logitech',
+			                	 done : false
+			                 },
+
+			                 {
+			                	 id: '25',
+			                	 name: 'logo4',
+			                	 star:3,
+			                	 ans:'volkswagen',
+			                	 done : false
+			                 },
+
+			                 {
+			                	 id: '26',
+			                	 name: 'logo5',
+			                	 star:3,
+			                	 ans:'network',
+			                	 done : false
+			                 },
+
+			                 {
+			                	 id: '27',
+			                	 name: 'logo6',
+			                	 star:3,
+			                	 ans:'adidas',
+			                	 done : false
+			                 }			         ]
 	};
 
 
--- app/js/logoquiz.js
+++ app/js/logoquiz.js
@@ -4,6 +4,7 @@ define(function(require){
 	var gridTmplt = require('text!html/grid.html');
 	var logoTmplt = require('text!html/logo.html');
 	var introTmplt = require('text!html/intro.html');
+	var levelTmplt = require('text!html/level.html');
 
 	var Logoquiz = function(obj){
 		if(localStorage.getItem('logos') !== null){
@@ -28,31 +29,74 @@ define(function(require){
 	Logoquiz.prototype = {
 		init: function(){
 			this.renderIntro();
-//			this.renderApp();
-//			this.addGridCellEvent();
-//			this.goBack();
-//			this.checkAns();
+			this.renderLevel();
+			this.renderApp();
+			this.addEventHandlers();
+			this.goBack();
+			this.checkAns();
+			this.manageRouting();
 		},
 		
 		renderIntro : function(){
 			$('.intro').html(Handlebars.compile(introTmplt)());
 		},
+		renderLevel : function(){
+			$('.levels').html(Handlebars.compile(levelTmplt)(this.logoData));
+		},
 
 		renderApp : function(){
-			$('.grid-container').html(Handlebars.compile(gridTmplt)(this.logoData));
+			$('.logo-container').html(Handlebars.compile(gridTmplt)(this.logoData));
 		},
 		
-		addGridCellEvent : function(){
+		addEventHandlers : function(){
+			var self = this;
 			$('.grid-container').on('click','.grid-cell',function(e){
 				var currItem = {};
 				currItem.name = $(this).data("name");
 				currItem.ans = $(this).data("ans");
 				currItem.id = $(this).data("id");
 				currItem.status = $(this).data("status");
-				$('.grid-container').html(Handlebars.compile(logoTmplt)(currItem));
+				$('.logo-container').html(Handlebars.compile(logoTmplt)(currItem));
 				e.stopPropagation();
 			});
+			
+			$('.app-container').on('click','a',function(){
+				setTimeout(function(){
+					self.manageRouting();
+				},100);
+			});
 		},
+		
+		goLevel : function(){
+			$('.intro').hide();
+			$('.levels').show();
+			$('.grid-container').hide();
+			$('.levels').removeClass('entry_anim');
+			$('.levels').addClass('entry_anim');
+			$('.level-container').removeClass('entry_anim1');
+			$('.level-container').addClass('entry_anim1');
+		},
+		
+		goMenu : function(){
+			$('.intro').show();
+			$('.levels').hide();
+			$('.grid-container').hide();
+			$('.intro').removeClass('entry_anim');
+			$('.intro').addClass('entry_anim');
+		},
+		
+		
+		goQuiz : function(){
+			$('.intro').hide();
+			$('.levels').hide();
+			$('.grid-container').show();
+			this.renderApp();
+			$('.grid-container').removeClass('entry_anim');
+			$('.grid-container').addClass('entry_anim');
+			$('.logo-container').removeClass('entry_anim1');
+			$('.logo-container').addClass('entry_anim1');
+		},
+		
 
 		goBack : function(){
 			var self = this;
@@ -83,6 +127,23 @@ define(function(require){
 						
 					}
 			});
+		},
+		
+		manageRouting : function(){
+			var route = window.location.hash;
+			console.log(route);
+			switch(route) {
+				case "#level":
+					this.goLevel();
+					break;
+				case "#grid":
+					this.goQuiz();
+					break;
+				case "#menu":
+					this.goMenu();
+					break;
+					
+			}
 		},
 		
 		updateData : function(itemId){
